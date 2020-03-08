05dc89b0b3c28ba408a95a3462e859a7fe11c8fb
SergeyLashenko <sergeylashenco@gmail.com>
2020-03-08 13:20:17 +0200

    Реализована возможность удалённого запуска браузера

--- src/test/java/org/example/app/Application.java
+++ src/test/java/org/example/app/Application.java
@@ -6,8 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Application {
@@ -20,6 +25,7 @@ public class Application {
    private PageCopyPhotoToBoard pageCopyPhotoToBoard;
    private MiniPageBoard miniPageBoard;
    private SaveToBoard saveToBoard;
+   private Properties properties;
 
    public Application(String browser) {
       this.browser = browser;
@@ -29,7 +35,7 @@ public class Application {
       this.wb  = wb;
    }
 
-   public void init() {
+   public void init() throws MalformedURLException {
       initDriver();
       wb.manage().window().maximize();
       wb.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -40,22 +46,30 @@ public class Application {
       pageCopyPhotoToBoard = new PageCopyPhotoToBoard(wb);
       miniPageBoard = new MiniPageBoard(wb);
       saveToBoard = new SaveToBoard(wb);
+      properties = new Properties();
    }
 
-   private void initDriver() {
-      if (browser == null || "chrome".equals(browser.toLowerCase())) {
-         File file = new File("./src/drivers/chromedriver");
-         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-         ChromeOptions options = new ChromeOptions();
-         options.addArguments("--disable-notifications");
-         wb = new ChromeDriver();
-      } else if ("firefox".equals(browser)) {
-         File file = new File("./src/drivers/geckodriver");
-         System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
-         FirefoxOptions profile = new FirefoxOptions();
-         profile.addArguments("--disable-notifications");
-         wb = new FirefoxDriver();
+   private void initDriver() throws MalformedURLException {
+      if( "".equals(properties.getProperty("selenium.server"))){
+         if (browser == null || "chrome".equals(browser.toLowerCase())) {
+            File file = new File("./src/drivers/chromedriver");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            wb = new ChromeDriver();
+         } else if ("firefox".equals(browser)) {
+            File file = new File("./src/drivers/geckodriver");
+            System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addArguments("--disable-notifications");
+            wb = new FirefoxDriver();
+         }
+      } else {
+         DesiredCapabilities capabilities = new DesiredCapabilities ();
+         capabilities.setBrowserName(browser);
+         wb = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities );
       }
+
    }
 
 
